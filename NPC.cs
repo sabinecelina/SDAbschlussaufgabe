eae846b3b34724d42152b987182d1171b292fbb6
Sabine Schleise <schleisesabine@yahoo.de>
2020-02-02 15:01:47 +0100

    NPCs added

---
+++ NPC.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace AdventureGame
+{
+    public class NPC
+    {
+        private string name;
+        private int healthpoints;
+        private int strength;
+        private int inventorySpace;
+        private string characteristics;
+
+    }
+}
