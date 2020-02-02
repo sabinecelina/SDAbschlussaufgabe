e3d6e9a3cac5ea59e619b198dc3d9a5d46d7b58e
Sabine Schleise <schleisesabine@yahoo.de>
2020-02-02 14:58:47 +0100

    Room added with four doors and an item list

--- Door.cs
+++ Door.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace AdventureGame
 {
     public class Door
---
+++ Room.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace AdventureGame
+{
+    public class Room
+    {
+        private Door north;
+        private Door east;
+        private Door south;
+        private Door west;
+        private List<string> inventory;
+
+    }
+}
